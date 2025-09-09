e1cc9eba6aed01a165fb40bd46f5c79ff30927a9
NyleLuza <Jluza21@gmail.com>
2025-09-09 01:31:17 -0700

    combined csv files

---
+++ data_prep.py
@@ -0,0 +1,25 @@
+import kagglehub
+import pandas as pd
+
+
+def combined_set(df, data):
+    for i in range(len(data)):
+        df = pd.concat([df, data[i].head(n = data[i].size)])
+    print(df.shape)
+    return df
+
+
+# Download latest version
+path = kagglehub.dataset_download("sudalairajkumar/cryptocurrencypricehistory")
+# initalize datasets
+bit_dataset = pd.read_csv(f"{path}/coin_Bitcoin.csv")
+eth_dataset = pd.read_csv(f"{path}/coin_Ethereum.csv")
+ada_dataset = pd.read_csv(f"{path}/coin_Cardano.csv")
+sol_dataset = pd.read_csv(f"{path}/coin_Solana.csv")
+doge_dataset = pd.read_csv(f"{path}/coin_Dogecoin.csv")
+dataset_array = [bit_dataset, eth_dataset,
+                 ada_dataset, sol_dataset, doge_dataset]
+main_dataset = pd.DataFrame()
+
+df = combined_set(main_dataset, dataset_array)
+print(df.head())
