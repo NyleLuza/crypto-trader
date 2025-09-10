e3361a31d0c00e043a95a0be996ab6f460df0b2b
NyleLuza <Jluza21@gmail.com>
2025-09-09 22:45:04 -0700

    discarded combining sets and focused on bitcoin

--- data_prep.py
+++ data_prep.py
@@ -2,14 +2,11 @@ import kagglehub
 import pandas as pd
 
 
-def combined_set(df, data):
-    for i in range(len(data)):
-        after_drop = data[i].drop(columns = ["SNo", "Symbol"], axis=1)
-        filter = after_drop[after_drop["Volume"]!=0]
-        print(filter)
-        df = pd.concat([df, after_drop.head(n = after_drop.size)])
-    #print(df.shape)
-    return df
+def combined_set(data):
+    after_drop = data.drop(columns = ["SNo", "Symbol", "Name", "Date"], axis=1)
+    filter = after_drop[after_drop["Volume"]!=0]
+    dropped_index = filter.reset_index(drop=True)
+    return dropped_index
 
 
 # Download latest version
@@ -20,10 +17,9 @@ eth_dataset = pd.read_csv(f"{path}/coin_Ethereum.csv")
 ada_dataset = pd.read_csv(f"{path}/coin_Cardano.csv")
 sol_dataset = pd.read_csv(f"{path}/coin_Solana.csv")
 doge_dataset = pd.read_csv(f"{path}/coin_Dogecoin.csv")
-dataset_array = [bit_dataset, eth_dataset,
-                 ada_dataset, sol_dataset, doge_dataset]
-main_dataset = pd.DataFrame()
 
-df = combined_set(main_dataset, dataset_array)
-pd.set_option("display.max_columns", 9)
-#print(df.head())
+
+
+df = combined_set(bit_dataset)
+
+print(df.head())
